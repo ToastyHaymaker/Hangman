0498138870af55bf6631e7dcd000b39153236217
ToastyHaymaker <31699772+ToastyHaymaker@users.noreply.github.com>
2018-09-23 21:19:59 -0400

    Skeleton
    Setting up the basics, getting all the filetypes I'll need and framing the fxml

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,9 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -10,7 +12,9 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			//loads panels and such from FXML File
+			Parent root = FXMLLoader.load(getClass().getResource("mainFXML.fxml"));
+			
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
